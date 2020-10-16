55a7f77dd46976a0efbfcabe9ac7bc3ea4a97f75
Lehuynhduc2203 <71062581+Lehuynhduc2203@users.noreply.github.com>
2020-10-16 11:04:34 +0700

    Update ChangePasswordHandler.java

--- src/main/java/com/app/server/handler/ChangePasswordHandler.java
+++ src/main/java/com/app/server/handler/ChangePasswordHandler.java
@@ -1,1 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.app.server.handler;
+
+import java.util.Date;
+import java.util.List;
+
+import com.app.models.ClipServices;
+import com.app.pojo.Users;
+import com.app.session.redis.SessionStore;
+import com.app.util.AppParams;
+import com.google.gson.Gson;
+
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.vertx.core.Handler;
+import io.vertx.core.json.JsonObject;
+import io.vertx.rxjava.core.http.HttpServerRequest;
+import io.vertx.rxjava.ext.web.RoutingContext;
+
+public class ChangePasswordHandler implements Handler<RoutingContext>, SessionStore {
+
+	private static ClipServices clipServices;
+
+	@Override
+	public void handle(RoutingContext routingContext) {
+
+		routingContext.vertx().executeBlocking(future -> {
+			try {
+				HttpServerRequest httpServerRequest = routingContext.request();
+				JsonObject jsonResponse = new JsonObject();
+				Gson gson = new Gson();
+				// lay tham so username, password tu path
+				JsonObject jsonRequest = routingContext.getBodyAsJson();
+				String sessionId = jsonRequest.getString("sessionId");
+				String oldPassword = jsonRequest.getString("oldPassword");
+				String newPassword = jsonRequest.getString("newPassword");
+				String confirmNewPassword = jsonRequest.getString("confirmNewPassword");
+				JsonObject data = new JsonObject();
+				// data.put("sessionId", sessionId);
+				data.put("message", "change password failed");
+				routingContext.put(AppParams.RESPONSE_CODE, HttpResponseStatus.UNAUTHORIZED.code());
+				routingContext.put(AppParams.RESPONSE_MSG, HttpResponseStatus.UNAUTHORIZED.reasonPhrase());
+				Users loggedInUser = gson.fromJson(jedis.get(sessionId), Users.class);
+				String email = loggedInUser.getEmail();
+				String password = loggedInUser.getPassword();
+				List<Users> list = clipServices
+						.findAllByProperty("from Users where email = '" + email + "'", null, 0, Users.class, 0);
+				if (list.size() > 0) {
+					if(oldPassword.equals(newPassword)) {
+						data.put("message", "change password failed: oldPassword and newPassword are duplicated");
+						routingContext.put(AppParams.RESPONSE_CODE, HttpResponseStatus.UNAUTHORIZED.code());
+						routingContext.put(AppParams.RESPONSE_MSG, HttpResponseStatus.UNAUTHORIZED.reasonPhrase());
+					}else {
+						if (newPassword.equals(confirmNewPassword)) {
+							Users resultUser = list.get(0);
+							
+								Date date = new Date();
+								Users newUser = new Users(resultUser.getId(), resultUser.getName(), email, newPassword,
+										resultUser.getCreatedAt(), date, resultUser.getLastLogin(), resultUser.getState(),
+										resultUser.getCountryCode());
+								clipServices.update(newUser, newUser.getId(), Users.class, 0);
+								data.put("message", "change password successed");
+								routingContext.put(AppParams.RESPONSE_CODE, HttpResponseStatus.OK.code());
+								routingContext.put(AppParams.RESPONSE_MSG, HttpResponseStatus.OK.reasonPhrase());
+							
+						} else {
+							data.put("message", "change password fail : newPassword and confirmNewPassword are not matched");
+						}
+					}
+					
+				}
+				routingContext.put(AppParams.RESPONSE_DATA, data);
+				future.complete();
+			} catch (Exception e) {
+				routingContext.fail(e);
+			}
+		}, asyncResult -> {
+			if (asyncResult.succeeded()) {
+				routingContext.next();
+			} else {
+				routingContext.fail(asyncResult.cause());
+			}
+		});
+	}
+
+	public static void setClipServices(ClipServices clipServices) {
+		ChangePasswordHandler.clipServices = clipServices;
+	}
+
+}
 
